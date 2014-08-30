4b4cf6e4316db57e01f754cc0b2f643ab66ae4af
abhink <kabhinav86@gmail.com>
2014-08-30 21:40:17 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,81 @@
+data = [{
+        name : "Home",
+        children: [{
+            name : "Favorite",
+            children: [],
+            link: '#'
+          }, {
+            name: "Recent",
+            children: [{
+                name: 'asd',
+                children: [{
+                    name: '123',
+                    children: [],
+                    link: '#'
+                  },
+                  {
+                    name: '456',
+                    children: [],
+                    link: '#'
+                  },
+                  {
+                    name: '789',
+                    children: [],
+                    link: '#'
+                  }
+                ],
+                link: '',
+              },
+              {
+                name: 'zxc',
+                children: [],
+                link: '#'
+              },
+              {
+                name: 'qwe',
+                children: [],
+                link: '#'
+              }]
+         }, {
+            name: "Active",
+            children: [],
+            link: '#'
+         }],
+        link: '',
+      }, {
+        name: "Brand",
+        children: [{
+            name : "Solution",
+            children: [],
+            link: '#'
+          }, {
+            name: "Insight",
+            children: [],
+            link: '#'
+         }, {
+            name: "Product",
+            children: [],
+            link: '#'
+         }, {
+            name: "NewProduct",
+            children: [],
+            link: '#'
+         }],
+        link: '',
+      }, {
+        name: "Performance",
+        children: [{
+            name : "Solution",
+            children: [],
+            link: '#'
+          }, {
+            name: "Resources",
+            children: [],
+            link: '#'
+         }, {
+            name: "Product",
+            children: [],
+            link: '#'
+         }],
+        link: '',
+      }];
